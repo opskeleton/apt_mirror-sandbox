4cb2715483c41db2ffb49e428661cb684fc7a0ab
ronen <narkisr@gmail.com>
2015-03-21 00:06:31 +0200

    moving to utopic and mkfsing /data

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,19 +1,21 @@
 node default {
 
-  class { 'apt_mirror': }
+  class { 'apt_mirror':
+    base_path  => '/data'
+  }
 
-  apt_mirror::mirror { 'trusty':
+  apt_mirror::mirror { 'utopic':
     mirror     => 'il.archive.ubuntu.com',
     os         => 'ubuntu',
-    release    => ['trusty', 'trusty-updates', 'trusty-backports'],
-    components => ['main', 'restricted', 'universe', 'multiverse'],
+    release    => ['utopic', 'utopic-updates', 'utopic-backports'],
+    components => ['main', 'restricted', 'universe', 'multiverse']
   }
 
   apt_mirror::mirror { 'extras':
     mirror     => 'extras.ubuntu.com',
     os         => 'ubuntu',
-    release    => ['trusty'],
-    components => ['main'],
+    release    => ['utopic'],
+    components => ['main']
   }
 
   include ::nginx
@@ -23,5 +25,14 @@ node default {
     www_root  => '/var/spool/apt-mirror/mirror/il.archive.ubuntu.com/ubuntu/pool/',
     autoindex => 'on'
   }
+
+  file{'/data':
+    ensure => directory,
+  } ->
+
+  mkfs::device {'/dev/vda':
+    dest   => '/data'
+  }
+
 
 }
