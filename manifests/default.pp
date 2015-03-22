3e1e9694d3da7da7e136304b9635f9bcd5c94b7a
ronen <narkisr@gmail.com>
2015-03-22 02:48:41 +0200

    fixing nginx using locations and adding security

--- manifests/default.pp
+++ manifests/default.pp
@@ -11,7 +11,8 @@ node default {
     mirror     => 'il.archive.ubuntu.com',
     os         => 'ubuntu',
     release    => [$release, "${release}-updates", "${release}-backports"],
-    components => ['main', 'restricted', 'universe', 'multiverse']
+    components => ['main', 'restricted', 'universe', 'multiverse'],
+    alt_arch   => ['i386']
   }
 
   apt_mirror::mirror { 'extras':
@@ -21,11 +22,33 @@ node default {
     components => ['main']
   }
 
+  apt_mirror::mirror { 'security.ubuntu.com':
+    mirror     => 'security.ubuntu.com',
+    os         => 'ubuntu',
+    release    => ["${release}-security"],
+    components => ['main', 'restricted', 'universe', 'multiverse'],
+  }
+
   include ::nginx
 
   nginx::resource::vhost { $::hostname:
+    ensure               => present,
+    use_default_location => false
+  }
+
+  nginx::resource::location { 'ubuntu':
     ensure    => present,
-    www_root  => "${base_path}/mirror/il.archive.ubuntu.com/ubuntu/pool/",
+    www_root  => "${base_path}/mirror/il.archive.ubuntu.com/",
+    location  => '/ubuntu',
+    vhost     => $::hostname,
+    autoindex => 'on'
+  }
+
+  nginx::resource::location { 'ubuntu-security':
+    ensure    => present,
+    www_root  => "${base_path}/mirror/security.ubuntu.com/",
+    location  => '/ubuntu',
+    vhost     => $::hostname,
     autoindex => 'on'
   }
 
